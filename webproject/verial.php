2165ac15a6d8002431d9e393d8cd4cc1cacfe199
zeynep <127024895+zeyyp@users.noreply.github.com>
2023-05-17 23:32:46 +0300

    commits
    www klasöründeki projemi yükledim. Localhost için oraya farklı klasöre kopyalayıp devam etmiştim.

---
+++ webproject/verial.php
@@ -0,0 +1,62 @@
+<br>
+<h2> veriler geldi </h2>
+
+<?php
+ 
+ 
+
+?>
+
+<table>
+    <tr>
+        <td>Ad </td>
+        <td> : </td>
+        <td> <?php echo $_POST["ad"] ?> </td>
+    </tr>
+
+
+    <tr>
+        <td>Soyad </td>
+        <td> : </td>
+        <td> <?php echo $_POST["soyad"] ?> </td>
+    </tr>
+
+    <tr>
+        <td>Öğrenci mi? </td>
+        <td> : </td>
+        <td> <?php echo $_POST["öğrencimezun"] ?> </td>
+    </tr>
+    <tr>
+        <td>Okul </td>
+        <td> : </td>
+        <td> <?php echo $_POST["okul"] ?> </td>
+    </tr>
+    <tr>
+        <td>Meslek </td>
+        <td> : </td>
+        <td> <?php echo $_POST["meslek"] ?> </td>
+    </tr>
+
+
+    <tr>
+        <td>Mesaj </td>
+        <td> : </td>
+        <td> <?php echo $_POST["mesaj"] ?> </td>
+    </tr>
+
+    <tr>
+        <td>E-posta </td>
+        <td> : </td>
+        <td> <?php echo $_POST["email"] ?> </td>
+    </tr>
+
+    <tr>
+        <td>Telefon </td>
+        <td> : </td>
+        <td> <?php echo $_POST["telefon"] ?> </td>
+    </tr>
+
+    
+
+</table>    
+
