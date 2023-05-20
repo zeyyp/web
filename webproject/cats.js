fba00aaf7be99b779c1d723d2d23ff3da22693b2
zeynep <127024895+zeyyp@users.noreply.github.com>
2023-05-20 23:52:24 +0300

    Add files via upload

---
+++ webproject/cats.js
@@ -0,0 +1,16 @@
+ let card= document.getElementById("card");
+ let url = "https://api.thecatapi.com/v1/images/search";
+ let data = async()=> (await fetch(url)).json();
+
+ let btn = document.getElementById("tıkla");
+
+ btn.onclick = ()=> {
+    data().then((res)=>{
+     let img = document.createElement("img");
+     img.src = res[0].url;
+     card.appendChild(img);
+    }).catch((err)=>{
+        alert(err);
+    });
+ }
+  
